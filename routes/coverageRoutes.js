6bd906d2f2eb5e4693bf1a5b5d7cde15324951fe
muvvadhanush <muvvadhanush007@gmail.com>
2026-02-13 16:34:35 +0530

    fix: rename service file to bust cache and resolve module not found

--- routes/coverageRoutes.js
+++ routes/coverageRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { categorizePages, calculateCoverage, getLaunchReadiness } = require('../services/coverage/knowledgeCoverageService');
+const { categorizePages, calculateCoverage, getLaunchReadiness } = require('../services/coverage/kCoverageService');
 
 /**
  * POST /:id/recalculate-coverage
